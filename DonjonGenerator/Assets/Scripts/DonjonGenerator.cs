396ac4033a4577402dc6f26dd1d8ef7c2d4354d7
Thibautguerin <thibaut.guerin@edu.devinci.fr>
2021-11-16 22:02:40 +0100

    Generate primary and secondary paths

--- DonjonGenerator/Assets/Scripts/DonjonGenerator.cs
+++ DonjonGenerator/Assets/Scripts/DonjonGenerator.cs
@@ -1,17 +1,8 @@
-using System.Collections;
+﻿using System.Collections;
 using System.Collections.Generic;
 using UnityEngine;
 
-enum Difficulty
-{
-    PEACEFULL,
-    EASY,
-    MEDIUM,
-    HARD,
-    BOSS
-}
-
-enum DoorPosition
+public enum DoorPosition
 {
     LEFT,
     RIGHT,
@@ -19,39 +10,524 @@ enum DoorPosition
     BOTTOM
 }
 
-enum PathType
+public enum PathType
 {
     PRINCIPAL,
     SECONDARY,
     ALTERNATIVE,
-    HIDDEN
+    SECRET
 }
 
-enum RoomType
+public enum RoomType
 {
+    START,
     NORMAL,
-    KEY
+    KEY,
+    BOSS
 }
 
-class RoomNode
+public class RoomNode
 {
-    PathType pathType;
-    RoomType roomType;
+    public PathType pathType = PathType.PRINCIPAL;
+    public RoomType roomType = RoomType.NORMAL;
 
-    Vector2Int position;
-    Difficulty difficulty;
-    List<DoorPosition> doorsPosition;
+    public Vector2Int position;
+    public List<DoorPosition> doorPosition = new List<DoorPosition>();
+    public List<Door.STATE> doorState = new List<Door.STATE>();
 
-    List<RoomNode> nextRooms;
-    List<RoomNode> previousRooms;
+    public LinkedListNode<RoomNode> nextSecondary;
 
-    RoomNode modified;
+    public LinkedListNode<RoomNode> previousComeBack;
+    public LinkedListNode<RoomNode> nextComeBack;
+
+    //public Room defaultRoom;
+    //public Room modifiedRoom;
 }
 
 public class DonjonGenerator : MonoBehaviour
 {
-    Dictionary<Vector2Int, RoomNode> Rooms;
-    int nbDoorsBeforeTheBoss = 2;
-    int nbRoomsBetweenDoorsMin = 1;
-    int nbRoomsBetweenDoorsMax = 3;
+    public int nbDoors = 2;
+    [Header("After Start")]
+    public int nbRoomsAfterStartMin = 4;
+    public int nbRoomsAfterStartMax = 6;
+    [Header("Principal Paths")]
+    public int nbRoomsBetweenDoorsMin = 3;
+    public int nbRoomsBetweenDoorsMax = 5;
+    [Header("Secondary Paths")]
+    public int nbRoomsSecondaryPathMin = 2;
+    public int nbRoomsSecondaryPathMax = 3;
+    [Header("Before Boss")]
+    public int nbRoomsBeforeBossMin = 4;
+    public int nbRoomsBeforeBossMax = 6;
+    
+
+    LinkedList<RoomNode> primaryRooms = new LinkedList<RoomNode>();
+    List<LinkedList<RoomNode>> secondaryRooms = new List<LinkedList<RoomNode>>();
+    List<LinkedListNode<RoomNode>> comeBackRooms = new List<LinkedListNode<RoomNode>>();
+    LinkedListNode<RoomNode> secretRoom;
+
+    Dictionary<Vector2Int, LinkedListNode<RoomNode>> roomsDico = new Dictionary<Vector2Int, LinkedListNode<RoomNode>>();
+
+    private void Awake()
+    {
+        nbRoomsAfterStartMax++;
+        nbRoomsBetweenDoorsMax++;
+        nbRoomsSecondaryPathMax++;
+        nbRoomsBeforeBossMax++;
+    }
+
+    private void Start()
+    {
+        Generate();
+        DisplayDonjon();
+    }
+
+    [ContextMenu("Generate Donjon")]
+    public void Generate()
+    {
+        primaryRooms.Clear();
+        secondaryRooms.Clear();
+        roomsDico.Clear();
+        comeBackRooms.Clear();
+        secretRoom = null;
+
+        int currentNbDoors = 0;
+        int nbRooms = Random.Range(nbRoomsAfterStartMin, nbRoomsAfterStartMax);
+        int currentNbRooms = 0;
+
+        bool noError = true;
+
+        GenerateFirstRoom(ref currentNbDoors, ref nbRooms, ref currentNbRooms);
+        
+        while (currentNbDoors <= nbDoors && noError)
+        {
+            noError = GeneratePrincipalRoom(ref currentNbDoors, ref nbRooms, ref currentNbRooms);
+        }
+
+        if (noError)
+        {
+            noError = GenerateSecondaryPath();
+            if (noError)
+            {
+                noError = GenerateComeBackPath();
+                if (noError)
+                {
+                    noError = GenerateSecretRoom();
+                }
+            }
+        }
+
+        if (!noError)
+        {
+            Debug.Log("<color=red>FAIL: Restart Generation !</color>");
+            Generate();
+        }
+    }
+
+    public void GenerateFirstRoom(ref int currentNbDoors, ref int nbRoomsBetweenDoors, ref int currentNbRoomsBetweenDoors)
+    {
+        RoomNode newRoom = new RoomNode();
+
+        newRoom.pathType = PathType.PRINCIPAL;
+        newRoom.roomType = RoomType.START;
+        newRoom.position = new Vector2Int(0, 0);
+
+        currentNbRoomsBetweenDoors++;
+
+        primaryRooms.AddLast(newRoom);
+        roomsDico.Add(newRoom.position, primaryRooms.Last);
+    }
+
+    public bool GeneratePrincipalRoom(ref int currentNbDoors, ref int nbRoomsBetweenDoors, ref int currentNbRoomsBetweenDoors)
+    {
+        bool positionUsed = true;
+        int nbIter = 0;
+        DoorPosition randomDirection = DoorPosition.BOTTOM;
+        Vector2Int newPosition = new Vector2Int();
+
+        while (positionUsed && nbIter <= 20)
+        {
+            randomDirection = (DoorPosition)Random.Range(0, 4);
+            switch (randomDirection)
+            {
+                case DoorPosition.LEFT:
+                    newPosition = primaryRooms.Last.Value.position - new Vector2Int(1, 0);
+                    if (roomsDico.ContainsKey(newPosition))
+                    {
+                        nbIter++;
+                        continue;
+                    }
+                    break;
+                case DoorPosition.RIGHT:
+                    newPosition = primaryRooms.Last.Value.position + new Vector2Int(1, 0);
+                    if (roomsDico.ContainsKey(newPosition))
+                    {
+                        nbIter++;
+                        continue;
+                    }
+                    break;
+                case DoorPosition.TOP:
+                    newPosition = primaryRooms.Last.Value.position + new Vector2Int(0, 1);
+                    if (roomsDico.ContainsKey(newPosition))
+                    {
+                        nbIter++;
+                        continue;
+                    }
+                    break;
+                case DoorPosition.BOTTOM:
+                    newPosition = primaryRooms.Last.Value.position - new Vector2Int(0, 1);
+                    if (roomsDico.ContainsKey(newPosition))
+                    {
+                        nbIter++;
+                        continue;
+                    }
+                    break;
+                default:
+                    break;
+            }
+            positionUsed = false;
+        }
+        if (positionUsed)
+        {
+            // Recommencer la génération
+            return false;
+        }
+
+        RoomNode newRoom = new RoomNode();
+        
+        newRoom.position = newPosition;
+        newRoom.pathType = PathType.PRINCIPAL;
+
+        primaryRooms.Last.Value.doorPosition.Add(randomDirection);
+        switch (randomDirection)
+        {
+            case DoorPosition.LEFT:
+                newRoom.doorPosition.Add(DoorPosition.RIGHT);
+                break;
+            case DoorPosition.RIGHT:
+                newRoom.doorPosition.Add(DoorPosition.LEFT);
+                break;
+            case DoorPosition.TOP:
+                newRoom.doorPosition.Add(DoorPosition.BOTTOM);
+                break;
+            case DoorPosition.BOTTOM:
+                newRoom.doorPosition.Add(DoorPosition.TOP);
+                break;
+            default:
+                break;
+        }
+
+        if (currentNbRoomsBetweenDoors >= nbRoomsBetweenDoors && currentNbDoors < nbDoors)
+        {
+            // Mettre une porte
+            primaryRooms.Last.Value.doorState.Add(Door.STATE.CLOSED);
+            newRoom.doorState.Add(Door.STATE.CLOSED);
+
+            currentNbRoomsBetweenDoors = 0;
+            currentNbDoors++;
+            if (currentNbDoors < nbDoors)
+                nbRoomsBetweenDoors = Random.Range(nbRoomsBetweenDoorsMin, nbRoomsBetweenDoorsMax);
+            else
+                nbRoomsBetweenDoors = Random.Range(nbRoomsBeforeBossMin, nbRoomsBeforeBossMax);
+        }
+        else if (currentNbRoomsBetweenDoors >= nbRoomsBetweenDoors && currentNbDoors == nbDoors)
+        {
+            // Mettre le Boss
+            newRoom.roomType = RoomType.BOSS;
+            primaryRooms.Last.Value.doorState.Add(Door.STATE.OPEN);
+            newRoom.doorState.Add(Door.STATE.OPEN);
+
+            currentNbRoomsBetweenDoors = 0;
+            nbRoomsBetweenDoors = Random.Range(nbRoomsBetweenDoorsMin, nbRoomsBetweenDoorsMax);
+            currentNbDoors++;
+        }
+        else
+        {
+            primaryRooms.Last.Value.doorState.Add(Door.STATE.OPEN);
+            newRoom.doorState.Add(Door.STATE.OPEN);
+        }
+        currentNbRoomsBetweenDoors++;
+
+        primaryRooms.AddLast(newRoom);
+        roomsDico.Add(newRoom.position, primaryRooms.Last);
+        return true;
+    }
+
+    public bool GenerateSecondaryPath()
+    {
+        LinkedListNode<RoomNode> currentPrincipalRoom = primaryRooms.Last;
+
+        int findDoor = 0;
+
+        while (currentPrincipalRoom != null)
+        {
+            if (currentPrincipalRoom.Value.doorState.Contains(Door.STATE.CLOSED))
+            {
+                ++findDoor;
+            }
+            
+            if (findDoor == 4)
+            {
+                return false;
+            }
+
+            if (findDoor == 2 || findDoor == 3)
+            {
+                LinkedList<RoomNode> tmpSecondaryRooms = new LinkedList<RoomNode>();
+                Dictionary<Vector2Int, LinkedListNode<RoomNode>> tmpRoomsDico = new Dictionary<Vector2Int, LinkedListNode<RoomNode>>();
+
+                LinkedListNode<RoomNode> currentRoom = currentPrincipalRoom;
+                int randomLenght = Random.Range(nbRoomsSecondaryPathMin, nbRoomsSecondaryPathMax);
+
+                bool secondaryPathFinded = true;
+
+                for (int i = 1; i <= randomLenght; i++)
+                {
+                    bool positionUsed = true;
+                    int nbIter = 0;
+                    DoorPosition randomDirection = DoorPosition.BOTTOM;
+                    Vector2Int newPosition = new Vector2Int();
+
+                    while (positionUsed && nbIter <= 15)
+                    {
+                        randomDirection = (DoorPosition)Random.Range(0, 4);
+                        switch (randomDirection)
+                        {
+                            case DoorPosition.LEFT:
+                                newPosition = currentRoom.Value.position - new Vector2Int(1, 0);
+                                if (roomsDico.ContainsKey(newPosition) || tmpRoomsDico.ContainsKey(newPosition))
+                                {
+                                    nbIter++;
+                                    continue;
+                                }
+                                break;
+                            case DoorPosition.RIGHT:
+                                newPosition = currentRoom.Value.position + new Vector2Int(1, 0);
+                                if (roomsDico.ContainsKey(newPosition) || tmpRoomsDico.ContainsKey(newPosition))
+                                {
+                                    nbIter++;
+                                    continue;
+                                }
+                                break;
+                            case DoorPosition.TOP:
+                                newPosition = currentRoom.Value.position + new Vector2Int(0, 1);
+                                if (roomsDico.ContainsKey(newPosition) || tmpRoomsDico.ContainsKey(newPosition))
+                                {
+                                    nbIter++;
+                                    continue;
+                                }
+                                break;
+                            case DoorPosition.BOTTOM:
+                                newPosition = currentRoom.Value.position - new Vector2Int(0, 1);
+                                if (roomsDico.ContainsKey(newPosition) || tmpRoomsDico.ContainsKey(newPosition))
+                                {
+                                    nbIter++;
+                                    continue;
+                                }
+                                break;
+                            default:
+                                break;
+                        }
+                        positionUsed = false;
+                    }
+                    if (positionUsed)
+                    {
+                        // Aller à la node précédente
+                        if (i > 1)
+                        {
+                            currentRoom.Value.doorPosition.RemoveAt(currentRoom.Value.doorPosition.Count - 1);
+                            currentRoom.Value.doorState.RemoveAt(currentRoom.Value.doorState.Count - 1);
+                            currentRoom.Value.nextSecondary = null;
+                        }
+                        secondaryPathFinded = false;
+                        break;
+                    }
+
+                    // ajouter une room (secondary path)
+
+                    RoomNode newRoom = new RoomNode();
+
+                    newRoom.position = newPosition;
+                    newRoom.pathType = PathType.SECONDARY;
+
+                    currentRoom.Value.doorPosition.Add(randomDirection);
+                    switch (randomDirection)
+                    {
+                        case DoorPosition.LEFT:
+                            newRoom.doorPosition.Add(DoorPosition.RIGHT);
+                            break;
+                        case DoorPosition.RIGHT:
+                            newRoom.doorPosition.Add(DoorPosition.LEFT);
+                            break;
+                        case DoorPosition.TOP:
+                            newRoom.doorPosition.Add(DoorPosition.BOTTOM);
+                            break;
+                        case DoorPosition.BOTTOM:
+                            newRoom.doorPosition.Add(DoorPosition.TOP);
+                            break;
+                        default:
+                            break;
+                    }
+
+                    currentRoom.Value.doorState.Add(Door.STATE.OPEN);
+                    newRoom.doorState.Add(Door.STATE.OPEN);
+
+                    if (i == randomLenght)
+                    {
+                        newRoom.roomType = RoomType.KEY;
+                    }
+                    tmpSecondaryRooms.AddLast(newRoom);
+                    tmpRoomsDico.Add(newRoom.position, tmpSecondaryRooms.Last);
+
+                    if (i == 1) // première salle du chemin secondaire
+                    {
+                        currentRoom.Value.nextSecondary = primaryRooms.Last;
+                        tmpSecondaryRooms.AddFirst(currentRoom.Value);
+                    }
+
+                    currentRoom = tmpSecondaryRooms.Last;
+                }
+
+                if (secondaryPathFinded)
+                {
+                    secondaryRooms.Add(tmpSecondaryRooms);
+                    foreach (KeyValuePair<Vector2Int, LinkedListNode<RoomNode>> tmpRoom in tmpRoomsDico)
+                    {
+                        roomsDico.Add(tmpRoom.Key, tmpRoom.Value);
+                    }
+                    findDoor = findDoor == 2 ? 0 : 1;
+                }
+            }
+            currentPrincipalRoom = currentPrincipalRoom.Previous;
+        }
+
+        if (findDoor >= 2)
+        {
+            return false;
+        }
+
+        return true;
+    }
+
+    public bool GenerateComeBackPath()
+    {
+        return true;
+    }
+
+    public bool GenerateSecretRoom()
+    {
+        return true;
+    }
+
+    public void DisplayDonjon()
+    {
+        Vector2Int positionMin = new Vector2Int(int.MaxValue, int.MaxValue);
+        Vector2Int positionMax = new Vector2Int(int.MinValue, int.MinValue);
+
+        foreach (KeyValuePair<Vector2Int, LinkedListNode<RoomNode>> room in roomsDico)
+        {
+            /*string Doors = "";
+            for (int i = 0; i < room.Value.Value.doorPosition.Count; ++i)
+            {
+                Doors += room.Value.Value.doorPosition[i] + ": " + room.Value.Value.doorState[i] + "\n";
+            }
+
+            Debug.Log("----[" + room.Key + "]----\n" +
+                "Room Type: " + room.Value.Value.roomType + "\n" +
+                "Path Type: " + room.Value.Value.pathType + "\n" +
+                "Doors:\n" +
+                Doors +
+                "---------------");*/
+
+            if (room.Value.Value.position.x < positionMin.x)
+                positionMin.x = room.Value.Value.position.x;
+            if (room.Value.Value.position.y < positionMin.y)
+                positionMin.y = room.Value.Value.position.y;
+            if (room.Value.Value.position.x > positionMax.x)
+                positionMax.x = room.Value.Value.position.x;
+            if (room.Value.Value.position.y > positionMax.y)
+                positionMax.y = room.Value.Value.position.y;
+        }
+
+        Vector2Int mapSize = (positionMax - positionMin);
+        Vector2Int mapAbsSize = mapSize;
+
+        mapAbsSize.x = Mathf.Abs(mapAbsSize.x);
+        mapAbsSize.y = Mathf.Abs(mapAbsSize.y);
+
+        string map = "";
+
+        for (int y = 0; y <= mapAbsSize.y; ++y)
+        {
+            for (int x = 0; x <= mapAbsSize.x; ++x)
+            {
+                LinkedListNode<RoomNode> roomNode;
+                if (roomsDico.TryGetValue(new Vector2Int(x, mapAbsSize.y - y) + positionMin, out roomNode))
+                {
+                    map += "[";
+
+                    if (roomNode.Value.doorState.Contains(Door.STATE.CLOSED) && roomNode.Next != null && roomNode.Previous != null)
+                    {
+                        map += "<color=orange>";
+                    }
+                    else
+                    {
+                        switch (roomNode.Value.roomType)
+                        {
+                            case RoomType.START:
+                                map += "<color=green>";
+                                break;
+                            case RoomType.NORMAL:
+                                map += "<color=yellow>";
+                                break;
+                            case RoomType.KEY:
+                                map += "<color=blue>";
+                                break;
+                            case RoomType.BOSS:
+                                map += "<color=red>";
+                                break;
+                            default:
+                                break;
+                        }
+                    }
+
+                    if (roomNode.Next != null)
+                    {
+                        Vector2Int dir = roomNode.Next.Value.position - roomNode.Value.position;
+                        if (dir == new Vector2Int(1, 0))
+                        {
+                            map += "►</color>";
+                        }
+                        else if (dir == new Vector2Int(-1, 0))
+                        {
+                            map += "◄</color>";
+                        }
+                        else if (dir == new Vector2Int(0, 1))
+                        {
+                            map += "▲</color>";
+                        }
+                        else
+                        {
+                            map += "▼</color>";
+                        }
+                    }
+                    else
+                    {
+                        map += "◆</color>";
+                    }
+
+                    map += ((int)roomNode.Value.pathType + 1) + "]\t";
+                }
+                else
+                {
+                    map += ". . . . .\t";
+                }
+            }
+            map += "\n";
+        }
+
+        Debug.Log(map);
+    }
+
+    // Pour le chemin de retour -> probabilité qui augment pour aller vers la salle start
 }
