d1d60ada063f3158ca7b0d144f2e383f77c9c052
Thibautguerin <thibaut.guerin@edu.devinci.fr>
2021-11-19 09:48:09 +0100

    Add fountain

---
+++ DonjonGenerator/Assets/Fountain.cs
@@ -0,0 +1,53 @@
+using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class Fountain : MonoBehaviour
+{
+    public Player.MovementParameters speedBoost;
+    public Player.MovementParameters stunnedSpeedBoost;
+
+    public float boostDuration = 6;
+    private float currentDuration = 0;
+
+    private Player.MovementParameters saveDefaultSpeed;
+    private Player.MovementParameters saveStunnedSpeed;
+
+    private bool boostActivated = false;
+    private bool canTakeLife = true;
+
+    private void Update()
+    {
+        if (boostActivated)
+        {
+            currentDuration = Mathf.Min(boostDuration, currentDuration + Time.deltaTime);
+            if (currentDuration == boostDuration)
+            {
+                Player.Instance.defaultMovement = saveDefaultSpeed;
+                Player.Instance.stunnedMovement = saveStunnedSpeed;
+                boostActivated = false;
+            }
+        }
+    }
+
+    private void OnTriggerEnter2D(Collider2D collision)
+    {
+        if (collision.CompareTag("Player"))
+        {
+            saveDefaultSpeed = Player.Instance.defaultMovement;
+            saveStunnedSpeed = Player.Instance.stunnedMovement;
+
+            Player.Instance.defaultMovement = speedBoost;
+            Player.Instance.stunnedMovement = stunnedSpeedBoost;
+
+            if (canTakeLife)
+            {
+                Player.Instance.life++;
+                canTakeLife = false;
+            }
+
+            currentDuration = 0;
+            boostActivated = true;
+        }
+    }
+}
